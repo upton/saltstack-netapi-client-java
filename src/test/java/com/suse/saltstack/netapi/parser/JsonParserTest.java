63da36bdb93e3b14a1751cd32871d930a5529f46
Kevin Walter <kevin.walter.private@googlemail.com>
2015-06-23 15:38:07 +0200

    add tests for optional single value json

--- src/test/java/com/suse/saltstack/netapi/parser/JsonParserTest.java
+++ src/test/java/com/suse/saltstack/netapi/parser/JsonParserTest.java
@@ -227,6 +227,20 @@ public class JsonParserTest {
         assertEquals(expected, result.maybeInts);
     }
 
+
+    @Test
+    public void testOptionalSingleValue() {
+        JsonParser<Optional<Integer>> parser =
+                new JsonParser<>(new TypeToken<Optional<Integer>>(){});
+        InputStream nullValue = this.getClass()
+                .getResourceAsStream("/single_null_value.json");
+        assertFalse(parser.parse(nullValue).isPresent());
+
+        InputStream intValue = this.getClass()
+                .getResourceAsStream("/single_int_value.json");
+        assertEquals(new Integer(123), parser.parse(intValue).get());
+    }
+
     @Test
     public void testSaltStackJobsWithArgsAsKwargsParser() throws Exception {
         InputStream is = this.getClass()
