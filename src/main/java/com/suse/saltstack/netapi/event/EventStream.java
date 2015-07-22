6ade4d128b0432db14c29e5b439c88a8dc16cb72
Duncan Mac-Vicar P <dmacvicar@suse.de>
2015-07-22 15:34:50 +0200

    port the server events to use org.atmosphere.wasync
    - supports websockets (need still some work)
    - support SSE
    - close() works correctly

--- src/main/java/com/suse/saltstack/netapi/event/EventStream.java
+++ src/main/java/com/suse/saltstack/netapi/event/EventStream.java
@@ -1,34 +1,28 @@
 package com.suse.saltstack.netapi.event;
 
 import com.suse.saltstack.netapi.config.ClientConfig;
-import com.suse.saltstack.netapi.exception.SaltStackException;
+import org.atmosphere.wasync.Client;
+import org.atmosphere.wasync.ClientFactory;
+import org.atmosphere.wasync.Decoder;
+import org.atmosphere.wasync.Encoder;
+import org.atmosphere.wasync.Event;
+import org.atmosphere.wasync.Function;
+import org.atmosphere.wasync.Request;
+import org.atmosphere.wasync.RequestBuilder;
+import org.atmosphere.wasync.Socket;
 
-import org.glassfish.jersey.client.ClientProperties;
-import org.glassfish.jersey.media.sse.EventInput;
-import org.glassfish.jersey.media.sse.InboundEvent;
-import org.glassfish.jersey.media.sse.SseFeature;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+
 
 /**
- * Event stream implementation based on GlassFish Jersey Server-Sent Events (SSE).
+ * Event stream implementation based on wAsync:
+ * (A WebSockets/HTTP Client Library for Asynchronous Communication)
  */
 public class EventStream implements AutoCloseable {
-
-    private final ExecutorService executor = Executors.newSingleThreadExecutor();
 
     /**
      * Listeners that are notified of a new events.
@@ -41,71 +35,21 @@ public class EventStream implements AutoCloseable {
     private ClientConfig config;
 
     /**
-     * Internal state flag designating whether event processing loop
-     * has started;
+     * wAsync implementation
      */
-    private boolean eventProcessingStarted = false;
-
-    /**
-     * Private reference to the Jersey SSE specific event stream
-     */
-    private EventInput eventInput;
+    private RequestBuilder request;
+    private Socket socket;
 
     /**
      * Constructor used to create this object.  Automatically starts
      * event processing.
      * @param config Contains the necessary details such as endpoint URL and
      *               authentication token required to create the request to obtain
-     *               the {@link EventInput} event stream.
+     *               the event stream.
      */
     public EventStream(ClientConfig config) {
         this.config = config;
         initializeStream();
-    }
-
-    /**
-     * An alternate constructor used to create this object.  Does not
-     * automatically start event processing.  This constructor is used for
-     * unit testing only and allows for the injection of an existing and
-     * possibly pre-configured {@link Client} object.
-     * @param config Contains the necessary details such as endpoint URL and
-     *               authentication token required to create the request to obtain
-     *               the {@link EventInput} event stream.
-     * @param client Passed client to use when configuring the event stream
-     */
-    public EventStream(ClientConfig config, Client client) {
-        client.register(new SseFeature());
-        WebTarget target = client.target(config.get(ClientConfig.URL) + "/events");
-        Invocation.Builder builder = target.request(new MediaType("text",
-                "event-stream"));
-        builder.header("X-Auth-Token", config.get(ClientConfig.TOKEN));
-
-        eventInput = builder.get(EventInput.class);
-    }
-
-    /**
-     * This method initiates the event processing loop. This method is intended for
-     * use during unit testing only!  End users should not call this methods directly.
-     */
-    public void processEvents() {
-        if (!eventProcessingStarted) {
-            if (eventInput != null && !eventInput.isClosed()) {
-                eventProcessingStarted = true;
-                while (!eventInput.isClosed()) {
-                    final InboundEvent inboundEvent = eventInput.read();
-                    if (inboundEvent == null) {
-                        // connection has been closed
-                        close();
-                        break;
-                    }
-                    synchronized (listeners) {
-                        for (EventListener listener : listeners) {
-                            listener.notify(inboundEvent.readData(String.class));
-                        }
-                    }
-                }
-            }
-        }
     }
 
     /**
@@ -150,31 +94,19 @@ public class EventStream implements AutoCloseable {
             }
             // clear out the listeners
             listeners.clear();
-            // close the backing event stream
-            if (eventInput != null) {
-                eventInput.close();
+
+            if (!isEventStreamClosed())  {
+                socket.close();
             }
-            // shut down the executor
-            executor.shutdownNow();
-            // reset processing flag
-            eventProcessingStarted = false;
         }
     }
 
     /**
      * Helper method to determine whether the backing event stream is closed.
-     * @return Whether the {@link EventInput} stream is closed.
+     * @return Whether the stream is closed.
      */
     public boolean isEventStreamClosed() {
-        return eventInput.isClosed();
-    }
-
-    /**
-     * Helper method to determine whether event processing has started.
-     * @return Whether event processing has started.
-     */
-    public boolean isEventProcessingStarted() {
-        return eventProcessingStarted;
+        return socket.status() == Socket.STATUS.CLOSE || socket.status() == Socket.STATUS.ERROR;
     }
 
     /**
@@ -182,46 +114,45 @@ public class EventStream implements AutoCloseable {
      * a proxy is configured be sure to account for it.
      */
     private void initializeStream() {
-        Callable<Object> callable = new Callable<Object>() {
-            @Override
-            public Object call() throws SaltStackException {
-                org.glassfish.jersey.client.ClientConfig jerseyConfig =
-                        new org.glassfish.jersey.client.ClientConfig();
-                // set the connection timeout as per
-                jerseyConfig.property(ClientProperties.CONNECT_TIMEOUT,
-                        config.get(ClientConfig.CONNECT_TIMEOUT));
 
-                // Configure jersey client for proxy if specified in configuration
-                URI uri = config.get(ClientConfig.URL);
-                String proxyHost = config.get(ClientConfig.PROXY_HOSTNAME);
-                if (proxyHost != null) {
-                    Integer proxyPort = config.get(ClientConfig.PROXY_PORT);
-                    try {
-                        URI proxyUri = new URI(uri.getScheme(), null, proxyHost,
-                                proxyPort, null, null, null);
-                        jerseyConfig.property(ClientProperties.PROXY_URI,
-                                proxyUri.toURL());
-                        jerseyConfig.property(ClientProperties.PROXY_USERNAME,
-                                config.get(ClientConfig.PROXY_USERNAME));
-                        jerseyConfig.property(ClientProperties.PROXY_PASSWORD,
-                                config.get(ClientConfig.PROXY_PASSWORD));
-                    } catch (URISyntaxException | MalformedURLException e) {
-                        throw new SaltStackException(e);
+        Client client = ClientFactory.getDefault().newClient();
+        request = client.newRequestBuilder()
+                .method(Request.METHOD.GET)
+                .uri(config.get(ClientConfig.URL) + "/events")
+                .encoder(new Encoder<String, Reader>() {
+                    @Override
+                    public Reader encode(String s) {
+                        return new StringReader(s);
+                    }
+                })
+                .decoder(new Decoder<String, String>() {
+                    @Override
+                    public String decode(Event type, String s) {
+                        return s;
+                    }
+                })
+                .transport(Request.TRANSPORT.SSE)
+                .header("X-Auth-Token", config.get(ClientConfig.TOKEN));
+
+        socket = client.create();
+
+        socket.on(new Function<String>() {
+            @Override
+            public void on(String s) {
+                synchronized (listeners) {
+                    for (EventListener listener : listeners) {
+                        listener.notify(s);
                     }
                 }
-
-                Client client = ClientBuilder.newClient(jerseyConfig).register(
-                        new SseFeature());
-                WebTarget target = client.target(uri + "/events");
-                Invocation.Builder builder = target.request(new MediaType("text",
-                        "event-stream"));
-                builder.header("X-Auth-Token", config.get(ClientConfig.TOKEN));
-
-                eventInput = builder.get(EventInput.class);
-                processEvents();
-                return null;
             }
-        };
-        executor.submit(callable);
+        });
+
+        try {
+            socket.open(request.build());
+        }
+        catch (IOException e) {
+            close();
+        }
+
     }
 }
---
+++ src/test/java/com/suse/saltstack/netapi/event/WasyncServerSentEventsTest.java
@@ -0,0 +1,166 @@
+package com.suse.saltstack.netapi.event;
+
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+import com.suse.saltstack.netapi.client.SaltStackClient;
+import com.suse.saltstack.netapi.utils.ClientUtils;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
+
+/**
+ * SaltStack events API Jersey SSE implementation test cases.
+ */
+public class WasyncServerSentEventsTest {
+
+    private static final int MOCK_HTTP_PORT = 8888;
+
+    /**
+     * Note: {@link org.atmosphere.wasync.Socket} expects the raw event
+     * stream to be \n\n delimited.  From the source code "SSE event chunk parser - SSE
+     * chunks are delimited with a fixed "\n\n" delimiter in the response stream."  The
+     * events_stream.txt file below is in the correct format.  If modifying the file to
+     * add additional events etc, please take note.
+     */
+    static final String TEXT_EVENT_STREAM_RESPONSE = ClientUtils.streamToString(
+            WasyncServerSentEventsTest.class.getResourceAsStream("/events_stream.txt"));
+
+    @Rule
+    public WireMockRule wireMockRule = new WireMockRule(MOCK_HTTP_PORT);
+
+    private SaltStackClient client;
+
+    @Before
+    public void init() {
+        URI uri = URI.create("http://localhost:" + Integer.toString(MOCK_HTTP_PORT));
+        client = new SaltStackClient(uri);
+    }
+
+    /**
+     * Tests: listener insertion, listener notification, stream closed,
+     * stream content
+     */
+    @Test
+    public void shouldFireNotifyMultipleTimes() {
+        stubFor(get(urlEqualTo("/events"))
+                .willReturn(aResponse()
+                .withHeader("Content-Type", "text/event-stream")
+                .withHeader("Connection", "keep-alive")
+                .withBody(TEXT_EVENT_STREAM_RESPONSE)));
+
+        try (EventStream serverSentEvents =
+                new EventStream(client.getConfig())) {
+            EventCountClient eventCountClient = new EventCountClient(6);
+            serverSentEvents.addEventListener(eventCountClient);
+
+            eventCountClient.await();
+        }
+    }
+
+
+    /**
+     * Event listener client used for testing.
+     */
+    private class EventCountClient implements EventListener {
+
+        private static final int TIMEOUT = 5;
+
+        private CountDownLatch latch;
+        private int targetCount;
+        private int counter = 0;
+
+        private List<String> events = new ArrayList<String>();
+
+        public EventCountClient(int targetCount) {
+            this.targetCount = targetCount;
+            this.latch = new CountDownLatch(targetCount);
+        }
+
+        // return all received events
+        public List<String> getEvents() {
+            return events;
+        }
+
+        @Override
+        public void notify(String event) {
+            this.events.add(event);
+            latch.countDown();
+            counter++;
+        }
+
+        @Override
+        public void eventStreamClosed() {
+            Assert.assertEquals(targetCount, counter);
+        }
+
+        public void await() {
+            try {
+                latch.await(TIMEOUT, TimeUnit.SECONDS);
+                Assert.assertEquals(targetCount, counter);
+            }
+            catch (InterruptedException e) {
+                Assert.fail("Timeout waiting for " + targetCount + " events");
+            }
+        }
+    }
+
+    /**
+     * Event listener client used for testing.
+     */
+    private class EventContentClient implements EventListener {
+        List<String> events = new ArrayList<>();
+
+        @Override
+        public void notify(String event) {
+            events.add(event);
+        }
+
+        @Override
+        public void eventStreamClosed() {
+            Assert.assertTrue(events.get(2).contains("\"jid\": \"20150505113307407682\""));
+        }
+    }
+
+    /**
+     * Simple Event ListenerClient
+     */
+    private class SimpleEventListenerClient implements EventListener {
+        @Override
+        public void notify(String event) { }
+
+        @Override
+        public void eventStreamClosed() { }
+    }
+
+    /**
+     * Event listener client used for testing.
+     */
+    private class EventStreamClosedClient implements EventListener {
+        EventStream eventSource;
+
+        public EventStreamClosedClient(EventStream eventSource) {
+            this.eventSource = eventSource;
+        }
+
+        @Override
+        public void notify(String event) {
+            Assert.assertFalse(eventSource.isEventStreamClosed());
+        }
+
+        @Override
+        public void eventStreamClosed() {
+        }
+    }
+}
